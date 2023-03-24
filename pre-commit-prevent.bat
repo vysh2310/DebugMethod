6107ef6c49b76337d46b4090322989e21c08d22b
vysh2310 <48465529+vysh2310@users.noreply.github.com>
2023-03-25 00:05:03 +0530

    Create pre-commit-prevent.bat

---
+++ pre-commit-prevent.bat
@@ -0,0 +1,19 @@
+@echo off
+
+setlocal enableextensions
+set "prohibited_words=Defect"
+
+for %%f in (%*) do (
+    if /i "%%~xf"==".csv" (
+        for /f "usebackq delims=" %%i in ("%%f") do (
+            for %%j in (%prohibited_words%) do (
+                echo %%i | find /i "%%j" > nul && (
+                    echo Commit contains prohibited words: %%j
+                    exit /b 1
+                )
+            )
+        )
+    )
+)
+
+exit /b 0
