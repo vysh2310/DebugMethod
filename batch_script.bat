2ff4ccc292f800f172bebf1cabe810cd91f1252b
vysh2310 <48465529+vysh2310@users.noreply.github.com>
2023-03-24 23:45:38 +0530

    Update batch_script.bat

--- batch_script.bat
+++ batch_script.bat
@@ -1,6 +1,6 @@
  echo "Running Polyspace analysis..."
  cd C:\Program Files\Polyspace\R2023a\polyspace\bin
- polyspace-bug-finder -sources Debugging.c -misra3 all -checkers all -results-dir C:\DebugGit\DebugMethod\temp1_analysis_result
+ polyspace-bug-finder -sources C:\DebugGit\DebugMethod\Debugging.c -misra3 all -checkers all -results-dir C:\DebugGit\DebugMethod\temp1_analysis_result
  polyspace-results-export -format csv ^
     -results-dir C:\DebugGit\DebugMethod\temp1_analysis_result ^
     -output-name C:\DebugGit\DebugMethod\temp1_analysis_result.csv
