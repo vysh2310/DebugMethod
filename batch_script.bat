63cc9c8cd85d5229df64f872a37289caeee43a82
vysh2310 <48465529+vysh2310@users.noreply.github.com>
2023-03-24 23:50:49 +0530

    Update batch_script.bat

--- batch_script.bat
+++ batch_script.bat
@@ -4,5 +4,23 @@
  polyspace-results-export -format csv ^
     -results-dir C:\DebugGit\DebugMethod\temp1_analysis_result ^
     -output-name C:\DebugGit\DebugMethod\temp1_analysis_result.csv
+  @echo off
+set "word_to_match=Defect"
+set "file_to_parse=temp1_analysis_result.csv"
+set "match_found="
+
+for /f "tokens=1-3 delims=," %%a in (%file_to_parse%) do (
+    if /i "%%b"=="%word_to_match%" (
+        set "match_found=1"
+        echo Match found: %%b
+    )
+)
+
+if defined match_found (
+    echo Failure: Prohibit commit due to defects
+    exit /b 1
+) else (
+    echo Success: Commit, as there are no defects
+)
 
 
