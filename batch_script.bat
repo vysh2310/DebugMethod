1e5a2d872ef7a3a91a0a595f76cba2a5b4c7c0ee
Vysh <vysh.simha@gmail.com>
2023-03-25 11:01:04 +0530

    removed variable b

--- batch_script.bat
+++ batch_script.bat
@@ -1,4 +1,4 @@
- echo "Running Polyspace analysis..."
+ echo "Polyspace analysis for any defects, do not commit"
  cd C:\Program Files\Polyspace\R2023a\polyspace\bin
  polyspace-bug-finder -sources C:\DebugGit\DebugMethod\Debugging.c -misra3 all -checkers all -results-dir C:\DebugGit\DebugMethod\temp1_analysis_result
  polyspace-results-export -format csv ^
